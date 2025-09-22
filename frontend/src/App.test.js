5e8f6674f17798d2775cffbd1e9f96e2b5e635e1
Max Ng <maxwindiff@gmail.com>
2025-09-22 00:11:48 -0700

    test

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Landing from './components/Landing';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders landing component without routing', () => {
+  render(<Landing />);
+  const titleElement = screen.getByText(/The Gang - Poker/i);
+  expect(titleElement).toBeInTheDocument();
 });
